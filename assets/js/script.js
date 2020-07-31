e262a87aac07c9425f79a381ceb40195557c5e25
Maca Müller <macarena.muller@ug.uchile.cl>
2020-07-31 00:48:17 -0400

    Vínculos del navegador con las diferentes secciones a través de smoth scroll, vínculos con redes sociales y vínculo curriculum vitae.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,18 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
   
 })
+
+$(function(){
+
+	$("a").click(function(event) {
+		if (this.hash !== "") {
+		event.preventDefault();
+		var gato = this.hash;
+		$('html, body').animate({
+		scrollTop: $(gato).offset().top
+		}, 1500, function(){
+		window.location.hash = gato;
+		});
+		} 
+	});
+});
